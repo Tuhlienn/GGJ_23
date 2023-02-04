c798eda23791131d482eb52d6de83ce6780e081d
Marian <marian@becher-home.net>
2023-02-04 09:53:08 +0100

    Add option for fixed nodes

--- Assets/Instructions/Node.cs
+++ Assets/Instructions/Node.cs
@@ -1,16 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class Node : MonoBehaviour,  IDragHandler
 {
-    [SerializeField] private Transform[] inputChannels;
-    [SerializeField] private Transform[] outputChannels;
+    [SerializeField] private bool draggable = true;
+    [SerializeField] private NodeChannel[] inputChannels;
+    [SerializeField] private NodeChannel[] outputChannels;
 
 
     public void OnDrag(PointerEventData eventData)
     {
+        if(!draggable)
+            return;
+            
         Vector3 pos = Camera.main.ScreenToWorldPoint(eventData.position);
         pos.z = 0;
         this.transform.position = pos;
